fc9be249cc3c1fbfc99317c708d6412c9f4c2d98
Madikhan <m.agatanov@vk.team>
2024-04-29 10:19:15 +0500

    http-server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
-const (
-	gauge   = "gauge"
-	counter = "counter"
-)
-
-var availableMetrics = map[string]struct{}{gauge: {}, counter: {}}
+var log = logrus.New()
 
 type MetricRepository interface {
 	addGaugeValues(string, string) error
 	addCounterValues(string, string) error
-	getGaugeValues(string) (float64, bool)
-	getCounterValues(string) (int64, bool)
 }
 
 type MemStorage struct {
 	gaugeValues   map[string]float64
 	counterValues map[string]int64
+}
+
+var storage = &MemStorage{
+	gaugeValues:   make(map[string]float64),
+	counterValues: make(map[string]int64),
 }
 
 func (m *MemStorage) addGaugeValues(key string, value string) error {
@@ -35,11 +33,6 @@ func (m *MemStorage) addGaugeValues(key string, value string) error {
 	return nil
 }
 
-func (m *MemStorage) getGaugeValues(key string) (float64, bool) {
-	value, found := m.gaugeValues[key]
-	return value, found
-}
-
 func (m *MemStorage) addCounterValues(key string, value string) error {
 	counterValue, err := strconv.Atoi(value)
 	if err != nil {
@@ -49,51 +42,67 @@ func (m *MemStorage) addCounterValues(key string, value string) error {
 	return nil
 }
 
-func (m *MemStorage) getCounterValues(key string) (int64, bool) {
-	value, found := m.counterValues[key]
-	return value, found
-}
-
 func gaugeMetricHandler(res http.ResponseWriter, req *http.Request) {
-
 	if req.Method != http.MethodPost {
 		http.Error(res, "Method is not allowed", http.StatusMethodNotAllowed)
+		log.Printf("Method not allowed: %s", req.Method)
 		return
 	}
-	urlPath := strings.Split(req.URL.Path, "/")[2:]
-	metricType := urlPath[0]
-	if _, found := availableMetrics[metricType]; !found {
-		http.Error(res, "incorrect metric type", http.StatusBadRequest)
+	urlPath := strings.Split(strings.Trim(req.URL.Path, "/"), "/")
+	if len(urlPath) < 4 {
+		http.Error(res, "incorrect input path", http.StatusBadRequest)
 		return
 	}
-	metricName := urlPath[1]
+	metricName, metricValue := urlPath[2], urlPath[3]
 	if metricName == "" {
-		http.Error(res, "incorrect metric name", http.StatusNotFound)
+		http.Error(res, "empty metric name", http.StatusNotFound)
 		return
 	}
-	metricValue := urlPath[2]
-	storage := &MemStorage{
-		gaugeValues:   make(map[string]float64),
-		counterValues: make(map[string]int64),
-	}
-	var err error
-	if metricType == gauge {
-		err = storage.addGaugeValues(metricName, metricValue)
-	} else {
-		err = storage.addCounterValues(metricName, metricValue)
-	}
+	err := storage.addGaugeValues(metricName, metricValue)
 	if err != nil {
 		http.Error(res, "incorrect metric value", http.StatusBadRequest)
 		return
 	}
-	fmt.Println(storage)
+	log.Info("Current storage state: ", storage)
+}
+
+func undefinedMetricType(res http.ResponseWriter, req *http.Request) {
+	http.Error(res, "incorrect metric type", http.StatusBadRequest)
+}
+
+func counterMetricHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Method is not allowed", http.StatusMethodNotAllowed)
+		log.Printf("Method not allowed: %s", req.Method)
+		return
+	}
+	urlPath := strings.Split(strings.Trim(req.URL.Path, "/"), "/")
+	if len(urlPath) < 4 {
+		http.Error(res, "incorrect input path", http.StatusNotFound)
+		return
+	}
+	metricName, metricValue := urlPath[2], urlPath[3]
+	if metricName == "" {
+		http.Error(res, "empty metric name", http.StatusNotFound)
+		return
+	}
+	err := storage.addCounterValues(metricName, metricValue)
+	if err != nil {
+		http.Error(res, "incorrect metric value", http.StatusBadRequest)
+		return
+	}
+	log.Info("Current storage state: ", storage)
+}
+
+func init() {
+	log.Formatter = &logrus.JSONFormatter{}
+	log.Level = logrus.InfoLevel
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/update/`, gaugeMetricHandler)
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
-		panic(err)
-	}
+	mux.HandleFunc(`/update/gauge/`, gaugeMetricHandler)
+	mux.HandleFunc(`/update/counter/`, counterMetricHandler)
+	mux.HandleFunc(`/update/`, undefinedMetricType)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
